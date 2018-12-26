321f83c651ab3e9ba8937fb9fba68da235ecd322
shubhiroy <shubham09roy@gmail.com>
2018-12-26 14:29:30 +0530

    Added Algorithmic Toolbox file with naive methods

---
+++ Algorithmic ToolBox/Algorithmic ToolBox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/FibonacciLastDigit.java
@@ -0,0 +1,30 @@
+//Naive method stores the whole number in fib[i] which need a lot of space and computation time and thus fails on large inputs as it goes integer Overflow.
+//Fast method stores only the last digit of the fibonacci no. (i.e. b/w 0-9). So the computation does not  goes integer Overflow.
+
+import java.util.*;
+
+public class FibonacciLastDigit {
+    private static int getFibonacciLastDigitNaive(int n) {
+        if (n <= 1)
+            return n;
+
+        int previous = 0;
+        int current  = 1;
+
+        for (int i = 0; i < n - 1; ++i) {
+            int tmp_previous = previous;
+            previous = current;
+            current = tmp_previous + current;
+        }
+
+        return current % 10;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int c = getFibonacciLastDigitNaive(n);
+        System.out.println(c);
+    }
+}
+
